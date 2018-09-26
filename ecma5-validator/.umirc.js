c0cad7b98560e4767209bc020834676c8268d146
sorrycc <sorrycc@gmail.com>
2018-09-26 18:03:28 +0800

    update

---
+++ ecma5-validator/.umirc.js
@@ -0,0 +1,5 @@
+
+export default {
+  plugins: ['umi-plugin-ecma5-validator']
+}
+
---
+++ ecma5-validator/pages/index.js
@@ -0,0 +1,11 @@
+import a from 'a';
+import styles from './index.css';
+
+export default function() {
+  a();
+  return (
+    <div className={styles.normal}>
+      <h1>Page index</h1>
+    </div>
+  );
+}
